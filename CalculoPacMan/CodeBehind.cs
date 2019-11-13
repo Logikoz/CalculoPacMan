8a7567fed83e558ae0a975799556a5c2d702a7cb
Logikoz <37851168+LogikozRC@users.noreply.github.com>
2019-11-13 17:17:04 -0500

    one class exemple.

--- CalculoPacMan/CodeBehind.cs
+++ CalculoPacMan/CodeBehind.cs
@@ -1,10 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace CalculoPacMan
 {
-    /// <summary>
-    /// Lógica interna para ResolverCalculo.xaml
-    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+        //ao passar por cima da moeda
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ale = new Random();
+            (int, int) numeros = (ale.Next(0, 10), ale.Next(0, 10));
+
+            _ = new ResolverCalculo(numeros).ShowDialog();
+        }
+    }
     public partial class ResolverCalculo : Window
     {
         private readonly (int n1, int n2) numeros;
